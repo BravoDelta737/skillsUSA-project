63219f0e6d2017105c1331228b4709fc9f1ce306
BravoDelta737 <jakelanc950@gmail.com>
2016-03-03 10:46:33 -0600

    Added comments to the code so others can actually understand it

--- src/com/main/mainprogram.java
+++ src/com/main/mainprogram.java
@@ -7,7 +7,9 @@ import java.util.Scanner;
 
 public class mainprogram {
 	public static void main(String[] args) throws FileNotFoundException{
+		//Creates an instance of the Scanner class to take keyboard input
 		Scanner kbReader = new Scanner(System.in);
+		//Creates vars for the contact info
 		String firstName;
 		String lastName;
 		String address;
@@ -15,10 +17,12 @@ public class mainprogram {
 		String state;
 		String zip;
 		String email;
+		//This bool makes the collect info loop, loop until the info correct
 		boolean incorrectInfo = true;
 		
 		while(incorrectInfo){
 			
+			//Gathers all the info from the user
 			firstName = getUserInfo(kbReader,"first name");
 			lastName = getUserInfo(kbReader,"last name");
 			address = getUserInfo(kbReader,"address");
@@ -27,6 +31,7 @@ public class mainprogram {
 			zip = getUserInfo(kbReader,"zip code");
 			email = getUserInfo(kbReader,"email");
 			
+			//Prints the info for the user to check
 			System.out.println(firstName + " " + lastName);
 			System.out.println(address);
 			System.out.println(city + " " + state + " " + zip);
@@ -35,18 +40,27 @@ public class mainprogram {
 			String response = kbReader.nextLine();
 			response.toLowerCase();
 			
+			//Asks the user if the information is correct
 			switch(response){
+			
 			case "yes":
+				//Setting this to false breaks the loop
 				incorrectInfo = false;
-				
+				//Creates the file <userlastname>.txt
 				createFile(lastName);
+				//Makes a String with the file name to pass to the write method
 				String writeFile = lastName + ".txt";
+				//Creates an instance of the file writer
 				PrintWriter writerFile = new PrintWriter(writeFile);
+				
+				//Writes each element to the file
 				writeToFile(writerFile,firstName + " " + lastName,"write");
 				writeToFile(writerFile,address,"write");
 				writeToFile(writerFile, city + ", " + state + " " + zip,"write");
 				writeToFile(writerFile, email,"write");
 				writeToFile(writerFile, null,"close");
+				
+				//Tells the user that everything worked and the program has ended
 				System.out.println("Elements have been written to the file. Goodbye.");
 			break;
 			
@@ -57,14 +71,17 @@ public class mainprogram {
 		}
 		
 	}
+	//Method collects user info
 	public static String getUserInfo(Scanner x, String infoName){
 		System.out.println("Please enter " + infoName);
 		return x.nextLine();
 	}
+	//Creates the file
 	public static void createFile (String x) throws FileNotFoundException{
 		Formatter fileFormat = new Formatter(x + ".txt");
 		System.out.println("File has been created");
 	}
+	
 	public static void writeToFile (PrintWriter file, String toWrite,String command) throws FileNotFoundException{
 		switch (command){
 		case "write":
